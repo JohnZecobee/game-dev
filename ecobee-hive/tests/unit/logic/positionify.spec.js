916c8f1044eceabce766e611dd660c27835711e2
Evan Burak <evanburak@gmail.com>
2018-07-18 17:56:50 -0400

    Deep freeze immutable fields in positionify test

--- ecobee-hive/tests/unit/logic/positionify.spec.js
+++ ecobee-hive/tests/unit/logic/positionify.spec.js
@@ -1,19 +1,27 @@
 import { expect } from "chai";
+import deepFreeze from "deep-freeze";
+
 import { positionify } from "@/logic/positionify";
 
 describe("positionify", () => {
   it("returns a single tile", () => {
     const tiles = [
-      { id: 0, type: "Ant", player: "white", neighbours: [null, null, null, null, null, null] }]
+      { id: 0, type: "Ant", player: "white", neighbours: [null, null, null, null, null, null] }
+    ]
+
+    deepFreeze(tiles);
     positionify(tiles)
     expect(tiles[0].x).to.equal(0);
     expect(tiles[0].y).to.equal(0);
   });
   it("returns a single row", () => {
       const tiles = [
-        { id: 0, type: "Ant", player: "white", neighbours: [null,    1, null, null, null, null] },
-        { id: 1, type: "Bee", player: "black", neighbours: [null,    2, null, null,    0, null] },
-        { id: 2, type: "Spider", player: "white", neighbours: [null, null, null, null,    1, null] }]
+        { id: 0, type: "Ant",    player: "white", neighbours: [null,    1, null, null, null, null] },
+        { id: 1, type: "Bee",    player: "black", neighbours: [null,    2, null, null,    0, null] },
+        { id: 2, type: "Spider", player: "white", neighbours: [null, null, null, null,    1, null] }
+      ]
+
+      deepFreeze(tiles);
       positionify(tiles)
       expect(tiles[0].x).to.equal(0);
       expect(tiles[0].y).to.equal(0);
@@ -24,9 +32,12 @@ describe("positionify", () => {
   });
   it("returns a single row out of order", () => {
     const tiles = [
-      { id: 0, type: "Ant", player: "white", neighbours: [null,    2, null, null, null, null] },
-      { id: 1, type: "Bee", player: "black", neighbours: [null, null, null, null,    2, null] },
-      { id: 2, type: "Spider", player: "white", neighbours: [null,    1, null, null,    0, null] }]
+      { id: 0, type: "Ant",    player: "white", neighbours: [null,    2, null, null, null, null] },
+      { id: 1, type: "Bee",    player: "black", neighbours: [null, null, null, null,    2, null] },
+      { id: 2, type: "Spider", player: "white", neighbours: [null,    1, null, null,    0, null] }
+    ]
+
+    deepFreeze(tiles);
     positionify(tiles)
     expect(tiles[0].x).to.equal(0);
     expect(tiles[0].y).to.equal(0);
