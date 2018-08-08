c40dbbda3fbd83148c3601c506523c96916c6cf2
John Ziolkowski <johnz@ecobee.com>
2018-08-08 18:07:27 -0400

    Add player hand test for one tile

--- ecobee-hive/tests/unit/components/playerHand.spec.js
+++ ecobee-hive/tests/unit/components/playerHand.spec.js
@@ -5,8 +5,26 @@ import PlayerHand from "@/components/PlayerHand.vue";
 describe("PlayerHand.Vue", () => {
   it("renders an empty PlayerHand", () => {
     const wrapper = shallowMount(PlayerHand, {
-      propsData: { tiles: [] }
+      propsData: {
+        player: "black",
+        tiles: []
+      }
     });
     expect(wrapper.props().tiles).to.be.empty;
+  });
+
+  it("a player hand with a single tile", () => {
+    const wrapper = shallowMount(PlayerHand, {
+      propsData: {
+        player: "black",
+        tiles: [
+          {
+            type: "Bee",
+            id: 1
+          }
+        ]
+      }
+    });
+    expect(wrapper.props().tiles.length).to.equal(1);
   });
 });
